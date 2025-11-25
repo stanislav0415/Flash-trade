2deffe12b13edec3c20fe7d285d22608a2f95d90
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-11-25 18:24:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import Header from './componets/header/Header';
+import Home from './componets/home/Home'
+import Footer from './componets/footer/Footer';
 function App() {
 
 
   return (
     <>
-       <Header />
-  <Routes>
-    <Route path='/' element={<Home />} />
-  </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
