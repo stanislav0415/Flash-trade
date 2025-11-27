9cde807ca3fe9916d831153832c741b1ec795888
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-11-27 19:11:43 +0200

    minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
-import Header from './componets/header/Header';
-import Home from './componets/home/Home'
-import Footer from './componets/footer/Footer';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 function App() {
-
-
   return (
     <>
       <Header />
+
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
