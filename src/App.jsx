a83db25fd4145b4a335720af8aab7fa473d1ba1d
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-11-28 13:02:41 +0200

    Refactor App.jsx and remove duplicate components
    Updated App.jsx to use AuthContext and conditional routes based on authentication state. Removed duplicate Footer, Header, and Home components from the incorrect 'componets' directory to avoid conflicts and ensure proper imports from the 'components' directory.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
+import { fetchUser } from './api/auth-api.js';
 import './App.css';
+import { AuthContext } from '../src/context/AuthContext.js';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+
 function App() {
+    const [authState, setAuthState] = useState({
+    email: '',
+    isAuthenticated: false,
+    isAdmin: false,
+  });
+    const changeAuthState = (state) => {
+    setAuthState(state);
+  };
+  useEffect(() => {
+    fetchUser(changeAuthState);
+  }, []);
+
+  const contextData = {
+    email: authState.email,
+    isAuthenticated: authState.isAuthenticated,
+    changeAuthState,
+    fetchUser
+  };
+
   return (
-    <>
+     <AuthContext.Provider value={contextData}>
       <Header />
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {!contextData.isAuthenticated ? (
+          <>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </>
+        ) : (
+          <>
+            
+          </>
+        )}
+        </Routes>
       <Footer />
-    </>
+     </AuthContext.Provider>
+
+  
   );
 }
 
